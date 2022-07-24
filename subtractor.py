8335d3b739077bc4280cfad179a4dab11c55d629
PeepeeHalpert <109461598+PeepeeHalpert@users.noreply.github.com>
2022-07-24 23:21:28 +0530

    Add files via upload

---
+++ subtractor.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Subtractor")
+
+st.write("This app can subtract numbers!")
+
+a = st.number_input("Enter the number you want to subtract from")
+
+b = st.number_input("Enter the number you want to subtract")
+
+st.write(a-b)
